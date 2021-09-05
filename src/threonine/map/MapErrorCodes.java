093ab5af8ae0b1253632b72c80a2516b6a4e95db
marscarda <52551131+marscarda@users.noreply.github.com>
2021-09-05 15:50:20 -0300

    New Map Error Codes

---
+++ src/threonine/map/MapErrorCodes.java
@@ -0,0 +1,9 @@
+package threonine.map;
+//*****************************************************************************
+public class MapErrorCodes {
+    public static final int MAPFOLDERNOTFOUND = 1000;
+    public static final int MAPRECORDNOTFOUND = 1001;
+    public static final int MAPRECORDNOTOBJ = 1002;
+    public static final int NODESQUANTITYEXCEDED = 1003;
+}
+//*****************************************************************************
