e0e5006861e3034ea2958e07296f60ea7eb72563
marscarda <52551131+marscarda@users.noreply.github.com>
2022-03-30 22:20:51 -0300

    Maps on templates Ok.

--- src/threonine/universe/TemplateAtlas.java
+++ src/threonine/universe/TemplateAtlas.java
@@ -9,6 +9,8 @@ import threonine.mapping.PointLocation;
 //**************************************************************************
 public class TemplateAtlas extends TemplateLock {
     //**********************************************************************
+    //UNIVERSES
+    //**********************************************************************
     public void addTemplateUniverse (Universe universe) throws Exception {
         //==================================================================
         if (wrmainsrv) connection = electra.mainSrvConnection();
@@ -24,56 +26,6 @@ public class TemplateAtlas extends TemplateLock {
             catch (SQLIntegrityConstraintViolationException e) {}
         }
         //==================================================================
-    }
-    //**********************************************************************
-    public void templateAddSubset (SubSet subset) throws Exception {
-        //==================================================================
-        if (wrmainsrv) connection = electra.mainSrvConnection();
-        else connection = electra.nearSrvConnection();
-        setDataBase();        
-        //==================================================================
-        while (true) {
-            try {
-                subset.subsetid = Celaeno.getUniqueID();
-                this.insertTemplateSubset(subset);
-                break;
-            }
-            catch (SQLIntegrityConstraintViolationException e) {}
-        }
-        //==================================================================
-    }
-    //**********************************************************************
-    /**
-     * 
-     * @param subsetid
-     * @param points
-     * @throws Exception 
-     */
-    public void templateAddFeature (long subsetid, PointLocation[] points) throws Exception {
-        //==================================================================
-        if (wrmainsrv) connection = electra.mainSrvConnection();
-        else connection = electra.nearSrvConnection();
-        setDataBase();
-        //==================================================================
-        MapFeature feature = new MapFeature();
-        feature.recordid = subsetid;
-        //------------------------------------------------------------------
-        while (true) {
-            try {
-                feature.objectid = Celaeno.getUniqueID();
-                this.insertTemplateFeature(feature);
-                break;
-            }
-            catch (SQLIntegrityConstraintViolationException e) {}
-        }
-        //------------------------------------------------------------------
-        //We insert the points for the object
-        for (PointLocation point : points) {
-            point.recordid = feature.recordid;
-            point.objectid = feature.objectid;
-            this.insertTemplatePointLocation(point);
-        }
-        //------------------------------------------------------------------
     }
     //**********************************************************************
     /**
@@ -118,7 +70,24 @@ public class TemplateAtlas extends TemplateLock {
         //------------------------------------------------------------------        
     }
     //**********************************************************************
-    
+    //** SUBSETS **
+    //**********************************************************************
+    public void templateAddSubset (SubSet subset) throws Exception {
+        //==================================================================
+        if (wrmainsrv) connection = electra.mainSrvConnection();
+        else connection = electra.nearSrvConnection();
+        setDataBase();        
+        //==================================================================
+        while (true) {
+            try {
+                subset.subsetid = Celaeno.getUniqueID();
+                this.insertTemplateSubset(subset);
+                break;
+            }
+            catch (SQLIntegrityConstraintViolationException e) {}
+        }
+        //==================================================================
+    }
     //**********************************************************************
     /**
      * Returns a subset.
@@ -139,7 +108,6 @@ public class TemplateAtlas extends TemplateLock {
         return subset;
     }    
     //**********************************************************************
-    
     public SubSet[] getSubsets (long universid, long parentid) throws Exception {
         //----------------------------------------------------------
         if (rdmainsrv) connection = electra.mainSrvConnection();
@@ -151,6 +119,64 @@ public class TemplateAtlas extends TemplateLock {
             subset.valid = true;
         return subsets;
     }    
+    //**********************************************************************
+    //MAP FEATURES
+    //**********************************************************************
+    /**
+     * 
+     * @param subsetid
+     * @param points
+     * @throws Exception 
+     */
+    public void templateAddFeature (long subsetid, PointLocation[] points) throws Exception {
+        //==================================================================
+        if (wrmainsrv) connection = electra.mainSrvConnection();
+        else connection = electra.nearSrvConnection();
+        setDataBase();
+        //==================================================================
+        MapFeature feature = new MapFeature();
+        feature.recordid = subsetid;
+        //------------------------------------------------------------------
+        while (true) {
+            try {
+                feature.objectid = Celaeno.getUniqueID();
+                this.insertTemplateFeature(feature);
+                break;
+            }
+            catch (SQLIntegrityConstraintViolationException e) {}
+        }
+        //------------------------------------------------------------------
+        //We insert the points for the object
+        for (PointLocation point : points) {
+            point.recordid = feature.recordid;
+            point.objectid = feature.objectid;
+            this.insertTemplatePointLocation(point);
+        }
+        //------------------------------------------------------------------
+    }
+    //**********************************************************************
+    /**
+     * Returns the maps features of a given subset.
+     * @param subsetid
+     * @param fillpoints
+     * @return
+     * @throws Exception 
+     */
+    public MapFeature[] getFeaturesBySubset (long subsetid, boolean fillpoints) throws Exception {
+        //----------------------------------------------------------
+        if (rdmainsrv) connection = electra.mainSrvConnection();
+        else connection = electra.nearSrvConnection();
+        setDataBase();
+        //----------------------------------------------------------        
+        MapFeature[] features = this.selectMapFeatures(subsetid);
+        if (!fillpoints) return features;
+        //------------------------------------------------------------------
+        for (MapFeature object : features)
+            object.points = this.selectPointLocations(object.objectid);
+        //------------------------------------------------------------------
+        return features;
+        //------------------------------------------------------------------
+    }
     //**********************************************************************
 }
 //**************************************************************************
--- src/threonine/universe/TemplateQ3.java
+++ src/threonine/universe/TemplateQ3.java
@@ -1,9 +1,18 @@
 package threonine.universe;
 //**************************************************************************
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import methionine.AppException;
+import methionine.sql.SQLCondition;
 import methionine.sql.SQLInsert;
+import methionine.sql.SQLOrderBy;
+import methionine.sql.SQLQueryCmd;
+import methionine.sql.SQLSelect;
+import methionine.sql.SQLWhere;
 import threonine.mapping.MapFeature;
 import threonine.mapping.PointLocation;
 //**************************************************************************
@@ -35,6 +44,57 @@ public class TemplateQ3 extends TemplateQ2 {
     }
     //**********************************************************************
     /**
+     * Selects and returns an array of map objects given a subset id
+     * @param subsetid
+     * @return
+     * @throws AppException
+     * @throws Exception 
+     */
+    protected MapFeature[] selectMapFeatures (long subsetid) throws AppException, Exception {
+        //-------------------------------------------------------
+        SQLQueryCmd sql = new SQLQueryCmd();
+        SQLSelect select = new SQLSelect(DBUniverse.SubsetMapFeatureTemplate.TABLE);
+        select.addItem(DBUniverse.SubsetMapFeatureTemplate.FEATUREID);
+        select.addItem(DBUniverse.SubsetMapFeatureTemplate.SUBSETID);
+        select.addItem(DBUniverse.SubsetMapFeatureTemplate.FEATURETYPE);
+        //-------------------------------------------------------
+        SQLWhere whr = new SQLWhere();
+        whr.addCondition(new SQLCondition(DBUniverse.SubsetMapFeatureTemplate.SUBSETID, "=", subsetid));
+        //-------------------------------------------------------
+        sql.addClause(select);
+        sql.addClause(whr);
+        //-------------------------------------------------------
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        //-------------------------------------------------------
+        try {
+            st = connection.prepareStatement(sql.getText());
+            sql.setParameters(st, 1);
+            rs = st.executeQuery();
+            List<MapFeature> objects = new ArrayList<>();
+            MapFeature object;
+            while (rs.next()) {
+                object = new MapFeature();
+                object.objectid = rs.getLong(DBUniverse.SubsetMapFeatureTemplate.FEATUREID);
+                object.recordid = rs.getLong(DBUniverse.SubsetMapFeatureTemplate.SUBSETID);
+                object.objtype = rs.getInt(DBUniverse.SubsetMapFeatureTemplate.FEATURETYPE);
+                objects.add(object);
+            }            
+            return objects.toArray(new MapFeature[0]);
+        }
+        catch (SQLException e) {
+            StringBuilder msg = new StringBuilder("Failed to select map objects for subset. Code: vrbcqwdrfh\n");
+            msg.append(e.getMessage());
+            throw new Exception(msg.toString());
+        }
+        finally {
+            if (st != null) try {st.close();} catch(Exception e){}
+            if (rs != null) try {rs.close();} catch(Exception e){}
+        }
+        //-------------------------------------------------------
+    }    
+    //**********************************************************************
+    /**
      * Inserts a new map point into de DB.
      * @param point
      * @throws Exception 
@@ -61,6 +121,63 @@ public class TemplateQ3 extends TemplateQ2 {
             if (st != null) try {st.close();} catch(Exception e){}
         }        
     }
-    //**********************************************************************    
+    //**********************************************************************
+    /**
+     * Selects and return point locations for an object.
+     * @param objectid
+     * @return
+     * @throws Exception 
+     */
+    protected PointLocation[] selectPointLocations (long objectid) throws Exception {
+        //-------------------------------------------------------
+        SQLQueryCmd sql = new SQLQueryCmd();
+        SQLSelect select = new SQLSelect(DBUniverse.TemplateLocationPoints.TABLE);
+        select.addItem(DBUniverse.TemplateLocationPoints.FEATUREID);
+        select.addItem(DBUniverse.TemplateLocationPoints.SUBSETID);
+        select.addItem(DBUniverse.TemplateLocationPoints.POINTINDEX);
+        select.addItem(DBUniverse.TemplateLocationPoints.LATITUDE);
+        select.addItem(DBUniverse.TemplateLocationPoints.LONGITUDE);
+        //-------------------------------------------------------
+        SQLWhere whr = new SQLWhere();
+        whr.addCondition(new SQLCondition(DBUniverse.TemplateLocationPoints.FEATUREID, "=", objectid));
+        //-------------------------------------------------------
+        SQLOrderBy order = new SQLOrderBy();
+        order.addColumn(DBUniverse.TemplateLocationPoints.POINTINDEX);
+        //-------------------------------------------------------
+        sql.addClause(select);
+        sql.addClause(whr);
+        sql.addClause(order);
+        //-------------------------------------------------------
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        //-------------------------------------------------------
+        try {
+            st = connection.prepareStatement(sql.getText());
+            sql.setParameters(st, 1);
+            rs = st.executeQuery();
+            List<PointLocation> points = new ArrayList<>();
+            PointLocation point;
+            while (rs.next()) {
+                point = new PointLocation();
+                point.objectid = objectid;
+                point.recordid = rs.getLong(DBUniverse.TemplateLocationPoints.SUBSETID);
+                point.ptindex = rs.getInt(DBUniverse.TemplateLocationPoints.POINTINDEX);
+                point.latitude = rs.getFloat(DBUniverse.TemplateLocationPoints.LATITUDE);
+                point.longitude = rs.getFloat(DBUniverse.TemplateLocationPoints.LONGITUDE);
+                points.add(point);
+            }            
+            return points.toArray(new PointLocation[0]);
+        }
+        catch (SQLException e) {
+            StringBuilder msg = new StringBuilder("Failed to select map point locations. Code: tyhgdytrfh\n");
+            msg.append(e.getMessage());
+            throw new Exception(msg.toString());
+        }
+        finally {
+            if (st != null) try {st.close();} catch(Exception e){}
+            if (rs != null) try {rs.close();} catch(Exception e){}
+        }
+    }
+     //**********************************************************************
 }
 //**************************************************************************
