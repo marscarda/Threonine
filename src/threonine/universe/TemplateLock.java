3ccf160393a38b28799cc8b81db2ee9b281e8da2
marscarda <52551131+marscarda@users.noreply.github.com>
2022-04-03 13:58:30 -0300

    Acquire template advance

--- src/threonine/universe/TemplateLock.java
+++ src/threonine/universe/TemplateLock.java
@@ -4,5 +4,8 @@ import methionine.TabList;
 //**************************************************************************
 public class TemplateLock extends TemplateQ3 {
     public void lockUniverseTemplate (TabList tabs) { tabs.addTable(databasename, DBUniverse.UniverseTemplate.TABLE); }
+    public void lockSubset (TabList tabs) { tabs.addTable(databasename, DBUniverse.SubsetTemplate.TABLE); }
+    public void lockMapFeature (TabList tabs) { tabs.addTable(databasename, DBUniverse.SubsetMapFeatureTemplate.TABLE); }
+    public void lockLocationPoint (TabList tabs) { tabs.addTable(databasename, DBUniverse.TemplateLocationPoints.TABLE); }    
 }
 //**************************************************************************
