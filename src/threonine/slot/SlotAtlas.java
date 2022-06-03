e6ca002a6e1ca55f91f03a9684c3ad573fb2efcf
marscarda <52551131+marscarda@users.noreply.github.com>
2022-06-03 15:24:05 -0300

    Sample set and remove from subsets OK

--- src/threonine/slot/SlotAtlas.java
+++ src/threonine/slot/SlotAtlas.java
@@ -1,6 +1,6 @@
 package threonine.slot;
 //**************************************************************************
-
+import methionine.AppException;
 //**************************************************************************
 public class SlotAtlas extends SlotQ1 {
     //**********************************************************************
@@ -17,6 +17,53 @@ public class SlotAtlas extends SlotQ1 {
         //==================================================================
         if (this.selectSubsetSlotCount(slot) != 0) this.updateSampleSlot(slot);
         else this.insertSlot(slot);
+        //==================================================================
+    }
+    //**********************************************************************
+    /**
+     * 
+     * @param universeid
+     * @param subsetid
+     * @return
+     * @throws Exception 
+     */
+    public SampleSlot getSlot (long universeid, long subsetid) throws Exception {
+        //==================================================================
+        if (rdmainsrv) connection = electra.mainSrvConnection();
+        else connection = electra.nearSrvConnection();
+        setDataBase();
+        //==================================================================
+        return this.selectSlot(universeid, subsetid);
+    }
+    //**********************************************************************
+    /**
+     * Set a subset free of samples.
+     * @param universeid
+     * @param subsetid
+     * @throws Exception 
+     */
+    public void freeSlot (long universeid, long subsetid) throws Exception {
+        //==================================================================
+        if (wrmainsrv) connection = electra.mainSrvConnection();
+        else connection = electra.nearSrvConnection();
+        setDataBase();
+        //==================================================================
+        this.deleteSlots(universeid, subsetid, 0, 0);
+        //==================================================================
+    }
+    //**********************************************************************
+    /**
+     * Clear all samples from a universe
+     * @param universeid
+     * @throws Exception 
+     */
+    public void clearSamples (long universeid) throws Exception {
+        //==================================================================
+        if (wrmainsrv) connection = electra.mainSrvConnection();
+        else connection = electra.nearSrvConnection();
+        setDataBase();
+        //==================================================================
+        this.deleteSlots(universeid, 0, 0, 0);
         //==================================================================
     }
     //**********************************************************************
--- src/threonine/slot/SlotQ1.java
+++ src/threonine/slot/SlotQ1.java
@@ -3,7 +3,9 @@ package threonine.slot;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import methionine.AppException;
 import methionine.sql.SQLCondition;
+import methionine.sql.SQLDelete;
 import methionine.sql.SQLInsert;
 import methionine.sql.SQLQueryCmd;
 import methionine.sql.SQLSelect;
@@ -34,7 +36,7 @@ public class SlotQ1 extends SlotTabs {
             msg.append(e.getMessage());
             throw new Exception(msg.toString());
         }
-        finally { if (st != null) try {st.close();} catch(Exception e){} }        
+        finally { if (st != null) try {st.close();} catch(Exception e){} }
     }    
     //**********************************************************************
     /**
@@ -107,6 +109,91 @@ public class SlotQ1 extends SlotTabs {
         }
         //-------------------------------------------------------
     }    
+    //**********************************************************************
+    /**
+     * 
+     * @param universeid
+     * @param subsetid
+     * @return
+     * @throws AppException
+     * @throws Exception 
+     */
+    protected SampleSlot selectSlot(long universeid, long subsetid) throws AppException, Exception {
+        SQLQueryCmd sql = new SQLQueryCmd();
+        SQLSelect select = new SQLSelect(DBSlot.SampleSlot.TABLE);
+        select.addItem(DBSlot.SampleSlot.UNIVERSEID);
+        select.addItem(DBSlot.SampleSlot.SUBSETID);
+        select.addItem(DBSlot.SampleSlot.SAMPLEID);
+        SQLWhere whr = new SQLWhere();
+        if (universeid != 0) whr.addCondition(new SQLCondition(DBSlot.SampleSlot.UNIVERSEID, "=", universeid));
+        whr.addCondition(new SQLCondition(DBSlot.SampleSlot.SUBSETID, "=", subsetid));
+        //-------------------------------------------------------
+        sql.addClause(select);
+        sql.addClause(whr);
+        //-------------------------------------------------------
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        //-------------------------------------------------------        
+        try {
+            st = connection.prepareStatement(sql.getText());
+            sql.setParameters(st, 1);
+            rs = st.executeQuery();
+            if (!rs.next())
+                throw new AppException("Sample Slot not found", SlotErrorCodes.SAMPLESLOTNOTFOUND);
+            SampleSlot slot = new SampleSlot();
+            slot.universeid = rs.getLong(DBSlot.SampleSlot.UNIVERSEID);
+            slot.subsetid = rs.getLong(DBSlot.SampleSlot.SUBSETID);
+            slot.sampleid = rs.getLong(DBSlot.SampleSlot.SAMPLEID);
+            return slot;
+        }
+        catch (SQLException e) {
+            StringBuilder msg = new StringBuilder("Failed to select sample slot. (fanterre)\n");
+            msg.append(e.getMessage());
+            throw new Exception(msg.toString());
+        }
+        finally {
+            if (st != null) try {st.close();} catch(Exception e){}
+            if (rs != null) try {rs.close();} catch(Exception e){}
+        }        
+    }
+    //**********************************************************************
+    /**
+     * Deletes slots given certain conditions
+     * @param universeid
+     * @param subsetid
+     * @param sampleid
+     * @param projectid
+     * @throws Exception 
+     */
+    protected void deleteSlots (long universeid, long subsetid, long sampleid, long projectid) throws Exception {
+        SQLQueryCmd sql = new SQLQueryCmd();
+        SQLDelete delete = new SQLDelete(DBSlot.SampleSlot.TABLE);
+        SQLWhere whr = new SQLWhere();
+        if (universeid != 0) whr.addCondition(new SQLCondition(DBSlot.SampleSlot.UNIVERSEID, "=", universeid));
+        if (subsetid != 0) whr.addCondition(new SQLCondition(DBSlot.SampleSlot.SUBSETID, "=", subsetid));
+        if (sampleid != 0) whr.addCondition(new SQLCondition(DBSlot.SampleSlot.SAMPLEID, "=", sampleid));
+        if (projectid != 0) whr.addCondition(new SQLCondition(DBSlot.SampleSlot.PROJECTID, "=", projectid));
+        //-------------------------------------------------------
+        sql.addClause(delete);
+        sql.addClause(whr);
+        //-------------------------------------------------------
+        PreparedStatement st = null;
+        //-------------------------------------------------------        
+        try {
+            st = connection.prepareStatement(sql.getText());
+            sql.setParameters(st, 1);
+            st.execute();            
+        }
+        catch (SQLException e) {
+            StringBuilder msg = new StringBuilder("Failed to delete subset slots (gartenwer)\n");
+            msg.append(e.getMessage());
+            throw new Exception(msg.toString());
+        }
+        finally { if (st != null) try {st.close();} catch(Exception e){} }        
+        
+        
+        
+    }
     //**********************************************************************
 }
 //**************************************************************************
